72a73eb21a8f0fbac6647833aedfbd38745e0cd9
Stephen <hubbard_stephen@yaho.com>
2019-11-14 16:49:35 -0700

    done with the day

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import './Add.css';
 
 export default class Add extends Component {
     state = {
@@ -9,8 +10,14 @@ export default class Add extends Component {
 
     render() {
         return(
-            <div>
-                Add.js
+            <div className="add">
+                <input placeholder="Title" type="text"/>
+                <input placeholder="Image Url" type="text"/>
+                <div className="img-container">
+                    <img src={this.state.imgUrl} alt="" />
+                </div>
+                <textarea placeholder="What do you want to say?" />
+                <button>Post</button>
             </div>
         )
     }
