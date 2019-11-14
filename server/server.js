8ab21a9e542e9aa372e4fd8a1579121b7c10f5df
Stephen <hubbard_stephen@yaho.com>
2019-11-14 15:49:20 -0700

    login works

--- server/server.js
+++ server/server.js
@@ -2,20 +2,25 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
+const authCtrl = require('./controllers/authController')
 
-const app = express() 
+const app = express()
 
 app.use(express.json())
 app.use(session({
     resave: false,
-    saveUninitialized: false, 
+    saveUninitialized: false,
     secret: SESSION_SECRET
 }))
 
+// AUTH ENDPOINTS
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+
 massive(CONNECTION_STRING).then(db => {
-    app.set(db, 'db')
-    app.listen(SERVER_PORT, () => 
-        console.log(`This server port is ${SERVER_PORT}`)
+    app.set('db', db)
+    app.listen(SERVER_PORT, () =>
+        console.log(`This server port is so ${SERVER_PORT}`)
     )
 })
